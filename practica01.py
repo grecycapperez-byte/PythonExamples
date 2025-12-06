be4760d56c29594cc73b18893aa30d20c9ed9851
grecia perez capilla <grecycaperez@gmail.com>
2025-12-05 19:21:16 -0600

    Suma dos variables tipo entero

---
+++ practica01.py
@@ -0,0 +1,9 @@
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def tambor(numero1,nuemero2):
+    return (numero1 + nuemero2)
+
+a = int (input('pon el primer numero:'))
+b = int (input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {tambor(a, b)}')
