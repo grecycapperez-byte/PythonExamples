6b5ed451e85e7b8c2b16bb6b3c85a9a54e2b75f2
Grey <grecycapperez@gmail.com>
2026-01-13 23:14:16 -0600

    Update practica02.py

--- practica02.py
+++ practica02.py
@@ -1,1 +1,6 @@
+"""En un escenario real, podrías usar None para indicar que algo no ha sido inicializado todavía."""
+# Type your code below
+nothing =  None
 
+# Don't change the line below
+print(f'nothing = {nothing}') 
