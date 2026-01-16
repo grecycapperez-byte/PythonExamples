c755ee0be5824061a02485c176afa19aaa385035
Grey <grecycapperez@gmail.com>
2026-01-15 20:51:39 -0600

    Add basic arithmetic operations in practica08.py
    sum, rest, multiplication, division

--- practica08.py
+++ practica08.py
@@ -1,8 +1,26 @@
 #Operaciones de matematicas basicas 
+#Suma
+t= 10
+q= 220
+k= t + q
+print(f"t= {t}, q= {q}, k={k} ");
+#Resta
+m=252 
+h=123
+o= m - y
+print(f" m= {m}, h= {h}, o={o}");
 
+#Multiplicacion 
+v= 123
+e= 5
+z= v * e
+print(f" v={v}, e={e}, z={z}");
+
+#Divicion 
 a= 5.2
 b= 2.6
 c= a / b
 
 # Don't change the line below
 print(f"a = {a}, b = {b}, c = {c}");
+
