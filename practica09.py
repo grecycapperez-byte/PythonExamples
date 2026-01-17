8bee5a3ed6dd37ea7f61eca1987b60dcf6faf3ca
Grey <grecycapperez@gmail.com>
2026-01-16 22:17:13 -0600

    Document set methods and provide example
    Added methods for set operations and example usage.

--- practica09.py
+++ practica09.py
@@ -1,6 +1,13 @@
 #Conjuntos 
 """Un conjunto es una estructura de datos mutable y no ordenada que permite almacenar una 
 colección de elementos únicos. Los conjuntos se encierran entre llaves {} o se crean utilizando la función set()."""
+#Metodos de conjuntos 
+""" 
+add(elemento): agrega un elemento al conjunto.
+remove(elemento): elimina un elemento del conjunto. Si el elemento no existe, genera un error.
+discard(elemento): elimina un elemento del conjunto si está presente. Si el elemento no existe, no hace nada.
+clear(): elimina todos los elementos del conjunto."""
+#Ejemplo
 frutas = {"manzana", "banana", "naranja"}
 
 
