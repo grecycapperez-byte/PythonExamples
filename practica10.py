9a87d6420446f4ceb5f5c7b4c3f819a4770c41af
Grey <grecycapperez@gmail.com>
2026-01-16 22:53:09 -0600

    Implement function for variable argument summation
    Added a function to sum a variable number of arguments.

--- practica10.py
+++ practica10.py
@@ -12,3 +12,17 @@ def sumar_3 (x):
   
 sumar = lambda x: x + 3
 print("Sumar 3 a un número:", sumar (5))
+
+"""Funciones con número variable de argumentos
+Python permite definir funciones que acepten un número variable de argumentos.
+Esto se logra utilizando el operador * antes del nombre del parámetro."""
+
+def suma_variable(*numeros):
+    total = 0
+    for numero in numeros:
+        total += numero
+    return total
+
+
+print(suma_variable(1, 2, 3))  # Imprime 6
+print(suma_variable(4, 5, 6, 7))  # Imprime 22
