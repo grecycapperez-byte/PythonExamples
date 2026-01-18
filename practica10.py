2e355f071793a2fa016937285c8192a5a636e312
Grey <grecycapperez@gmail.com>
2026-01-17 18:19:00 -0600

    Add suma_variable function to calculate sum

--- practica10.py
+++ practica10.py
@@ -26,3 +26,14 @@ def suma_variable(*numeros):
 
 print(suma_variable(1, 2, 3))  # Imprime 6
 print(suma_variable(4, 5, 6, 7))  # Imprime 22
+
+
+def suma_variable(*numeros):
+    total = 0
+    for numero in numeros:
+        total += numero
+    return total
+
+
+print(suma_variable(1, 2, 3))  # Imprime 6
+print(suma_variable(4, 5, 6, 7))  # Imprime 22
