fb2ae14e52a9bbfd78bcf470d31f4118fa3643bc
Grey <grecycapperez@gmail.com>
2026-01-18 23:09:28 -0600

    Implement exception handling examples in practiica11.py
    Added examples of exception handling in Python.

---
+++ practiica11.py
@@ -0,0 +1,29 @@
+#manejo de excepciones 
+
+#TRY
+try:
+    # Código que puede generar una excepción
+    resultado = 10 / 0  # División por cero
+    print(resultado)
+except ZeroDivisionError:
+    print("Error: División por cero")
+
+#EXCEPT
+try:
+    # Código que puede generar una excepción
+    resultado = 10 / 0  # División por cero
+    print(resultado)
+except ZeroDivisionError:
+    print("Error: División por cero")
+except ValueError:
+    print("Error: Valor inválido")
+  
+#FINALY
+try:
+    # Código que puede generar una excepción
+    archivo = open("archivo.txt", "r")
+    # Realizar operaciones con el archivo
+except FileNotFoundError:
+    print("Error: Archivo no encontrado")
+finally:
+    archivo.close()  # Cerrar el archivo siempre, incluso si ocurre una excepción
