6b87c49bf3b83c7196eccc3dc899e478a79bf022
Grey <grecycapperez@gmail.com>
2026-01-19 23:03:29 -0600

    Update practica12.py

--- practica12.py
+++ practica12.py
@@ -13,3 +13,10 @@ if edad >= 18:
     print("Eres mayor de edad.")
 else:
     print("Eres menor de edad.") 
+#ejemplo 
+
+nombre = "Juan"
+edad = 25
+
+
+print(f"Hola, mi nombre es {nombre} y tengo {edad} años.")
