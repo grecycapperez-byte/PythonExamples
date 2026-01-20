957db05170c47432bf839da865f1dcc2e95730ad
Grey <grecycapperez@gmail.com>
2026-01-19 23:02:08 -0600

    Add user input and age validation in practica12.py

---
+++ practica12.py
@@ -0,0 +1,15 @@
+#Entradas y salidas 
+nombre = input("Ingresa tu nombre: ")
+edad = input("Ingresa tu edad: ")
+
+
+print("Hola, " + nombre + "!")
+print("Tienes " + edad + " años.")
+
+#Ejemplo 
+
+edad = int(input("Ingresa tu edad: "))
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.") 
