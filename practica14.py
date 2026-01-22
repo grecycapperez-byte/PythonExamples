9e2aa3812e14ed907d0403728ff9b491000026ab
Grey <grecycapperez@gmail.com>
2026-01-21 22:58:37 -0600

    Update practica14.py

--- practica14.py
+++ practica14.py
@@ -1,7 +1,16 @@
+#Para importar modulos 
+"""Para utilizar un módulo 
+en nuestro programa, 
+debemos importarlo utilizando 
+la declaración import. Podemos importar un módulo 
+completo o funciones específicas de un módulo."""
 import math
 resultado = math.sqrt(25)
 print(resultado)  # Imprime 5.0
 
+"""También podemos importar funciones específicas 
+de un módulo utilizando la sintaxis
+from módulo import función."""
 from math import sqrt
 resultado = sqrt(25)
 print(resultado)  # Imprime 5.0
@@ -15,3 +24,5 @@ print(numero_aleatorio)  # Imprime un número entero aleatorio entre 1 y 10
 
 fecha_actual = datetime.datetime.now()
 print(fecha_actual)  # Imprime la fecha y hora actual
+
+
