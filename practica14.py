5c93931dd68e5578f2c8c45297046ff7564972e8
Grey <grecycapperez@gmail.com>
2026-01-21 23:01:58 -0600

    Add docstring for datetime functionalities
    Added a docstring explaining datetime functionalities.

--- practica14.py
+++ practica14.py
@@ -14,10 +14,12 @@ from módulo import función."""
 from math import sqrt
 resultado = sqrt(25)
 print(resultado)  # Imprime 5.0
+
+"""Permite trabajar con fechas y horas,
+como datetime.now() (fecha y hora actual), 
+datetime.date() (fecha), datetime.time() (hora), entre otras."""
 import random
 import datetime
-
-
 numero_aleatorio = random.randint(1, 10)
 print(numero_aleatorio)  # Imprime un número entero aleatorio entre 1 y 10
 
