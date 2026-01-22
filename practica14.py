eab6d73f24debdbf3e6c03832ec017df34c34835
Grey <grecycapperez@gmail.com>
2026-01-21 22:39:41 -0600

    Add mathematical and random functions to practica14.py

---
+++ practica14.py
@@ -0,0 +1,17 @@
+import math
+resultado = math.sqrt(25)
+print(resultado)  # Imprime 5.0
+
+from math import sqrt
+resultado = sqrt(25)
+print(resultado)  # Imprime 5.0
+import random
+import datetime
+
+
+numero_aleatorio = random.randint(1, 10)
+print(numero_aleatorio)  # Imprime un número entero aleatorio entre 1 y 10
+
+
+fecha_actual = datetime.datetime.now()
+print(fecha_actual)  # Imprime la fecha y hora actual
