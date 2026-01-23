81a92f9ba3844d6a492eb501cbf477a529920acc
Grey <grecycapperez@gmail.com>
2026-01-22 23:32:21 -0600

    Add comment on code organization in practica15.3.py
    Added a comment about code organization in modules.

---
+++ practica15.3.py
@@ -0,0 +1,17 @@
+# operaciones.py
+#Organización del código en módulos
+def sumar(a, b):
+    return a + b
+
+
+def restar(a, b):
+    return a - b
+
+
+# utilidades.py
+def imprimir_mensaje(mensaje):
+    print(mensaje)
+
+
+def obtener_nombre_usuario():
+    return input("Ingresa tu nombre: ")
