2d20e77fb5dfdfd28ede5650f35d1a408fbc5305
Grey <grecycapperez@gmail.com>
2026-01-22 23:31:13 -0600

    Add functionality to greet and calculate sum

---
+++ practica15.2.py
@@ -0,0 +1,7 @@
+#importacion de modulo segunda parte 
+import mi_modulo
+
+
+mi_modulo.saludar("Juan")  # Imprime "Hola, Juan!"
+resultado = mi_modulo.calcular_suma(5, 3)
+print(resultado)  # Imprime 8
