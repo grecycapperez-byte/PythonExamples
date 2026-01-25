24c1bfdfe93b2cd41ebf01d056e710f90d99a23a
Grey <grecycapperez@gmail.com>
2026-01-24 22:59:36 -0600

    Add modulo operations and print outputs
    Implement modulo operation and print results.

---
+++ practica17.py
@@ -0,0 +1,18 @@
+#Modulo Operator
+#El operador módulo % te dice qué queda después de dividir un número por otro.
+# Type your code below
+a = 9
+b = 2
+c = 11
+
+d= a%2 
+e= b%3
+f= c%10
+
+# Don't change the line below
+print(f"a = {a}")
+print(f"b = {b}")
+print(f"c = {c}")
+print(f"d = {d}")
+print(f"e = {e}")
+print(f"f = {f}")
