f84fa96c674aabcbcd3f151a3a32a451ed734e24
Grey <grecycapperez@gmail.com>
2026-01-25 23:10:00 -0600

    Add arithmetic shortcuts for count variable

--- practica15.4.py
+++ practica15.4.py
@@ -9,3 +9,12 @@ utilidades.imprimir_mensaje(f"El resultado de la suma es: {resultado}")
 
 nombre = utilidades.obtener_nombre_usuario()
 utilidades.imprimir_mensaje(f"Hola, {nombre}!")
+
+#Atajos aritmeticos 
+# Type your code below
+count = 0
+count =4
+count*=2
+count -=1
+# Don't change the line below
+print(f"count = {count}")
