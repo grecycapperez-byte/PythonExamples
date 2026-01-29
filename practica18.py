68295d721d7a0a3ec2273ea96739e5cd993bff7d
Grey <grecycapperez@gmail.com>
2026-01-28 22:34:22 -0600

    promedio

---
+++ practica18.py
@@ -0,0 +1,6 @@
+#Tienes 3 calificaciones de un alumno: 8.5, 9.0 y 7.5. Debes calcular el promedio final.
+Alum1=8.5
+Alum2=9.0
+Alum3=7.5
+alumnos= (Alum1 + Alum2 + Alum3)/3 
+print("El promedio final es:", alumnos)
