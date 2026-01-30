0f4c55aac47f0794de22c2cc0c7f947513c7814d
Grey <grecycapperez@gmail.com>
2026-01-29 22:51:09 -0600

    Update practica18.py

--- practica18.py
+++ practica18.py
@@ -4,3 +4,12 @@ Alum2=9.0
 Alum3=7.5
 alumnos= (Alum1 + Alum2 + Alum3)/3 
 print("El promedio final es:", alumnos)
+
+#Area y perimetro 
+#Usar fórmulas matemáticas básicas ($Area = base \times altura$)
+base= 20
+altura=15
+area= base * altura
+perimetro= (2 * base)+(2 * altura)
+print("El area es de:",area)
+print("El perimetro es de:",perimetro)
