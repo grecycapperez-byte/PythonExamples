53002a560743cfedadf151137523e3da17a710ee
Grey <grecycapperez@gmail.com>
2026-02-15 22:00:18 -0600

    Refactor revisar_presupuesto function and main program

--- practica21.py
+++ practica21.py
@@ -1,6 +1,6 @@
 def revisar_presupuesto(lista_gastos, limite):
     # Paso A: Sumamos los gastos
-    total = sum(lista_gastos) # sum() es una función de Python que suma listas rápido
+    total = sum(lista_gastos) 
     
     # Paso B: Calculamos la diferencia
     balance = limite - total
@@ -8,20 +8,16 @@ def revisar_presupuesto(lista_gastos, limite):
     # Paso C: Devolvemos el resultado
     return balance
 
-# --- AQUÍ EMPIEZA EL PROGRAMA PRINCIPAL ---
-
 mi_limite = 1000
 mis_gastos = [200, 150, 500]
 
-# Llamamos a la función y guardamos el resultado en 'resultado'
+
 resultado = revisar_presupuesto(mis_gastos, mi_limite)
 
 if resultado > 0:
     print(f"Te sobran ${resultado}")
 else:
-    print(f"Te faltan ${abs(resultado)}") # abs() convierte números negativos a positivos
-
-
+    print(f"Te faltan ${abs(resultado)}") 
 
 
 
