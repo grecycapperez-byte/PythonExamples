0927ecc4c002b086609886a340406201a59cf584
Grey <grecycapperez@gmail.com>
2026-02-15 22:28:17 -0600

    Initialize vacation budget variables in practica22.py

---
+++ practica22.py
@@ -0,0 +1,5 @@
+premax= 5000.0 
+nombre="Vacaciiones en familia"
+dinero_disponible = floa(inpunt("Con cuanto dinero cuentas realmente para este viaje?"))
+gastos=[90.00,150.00,200.50]
+                         
