9376053c4a8a31797859bb14627ad9cd9b978af4
Grey <grecycapperez@gmail.com>
2026-03-03 23:54:08 -0600

    Refactor code and add chocolate cake sales check
    Fixed indentation and added bakery logic for chocolate cake sales.

--- practica01.py
+++ practica01.py
@@ -13,3 +13,12 @@ t=150
 j= k/t
 print("La divicion de hambos numeros es de:",j)
 
+#panaderia el Marce 
+pastel= intput("Tines pastel de chocolate?")
+if pastel==chocolate
+print("Felicidades ya vendiste el pastel de chocolate! ")
+else:
+print("No tenias pastel de chocolate")
+
+
+
