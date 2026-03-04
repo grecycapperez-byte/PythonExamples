aed592575ddfed36b9006164a8ced0ef44ae6ad0
Grey <grecycapperez@gmail.com>
2026-03-03 23:49:04 -0600

    Update practica01.py

--- practica01.py
+++ practica01.py
@@ -7,3 +7,9 @@ a = int (input('pon el primer numero:'))
 b = int (input('Enter 2nd number: '))
 
 print(f'Sum of {a} and {b} is {tambor(a, b)}')
+
+k=20
+t=150
+j= k/t
+print("La divicion de hambos numeros es de:",j)
+
